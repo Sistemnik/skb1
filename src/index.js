3d0df4424640402064eeea61db62ef427b654fc8
Sergei Andreevich <sergei.andreevich74@yandex.ru>
2016-11-11 21:58:51 +0500

    A+B

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+
+import express from 'express'
+
+import cors from 'cors'
+
+//import canonize from './canonize'
+
+
+//var express = require('express');
+ var app = express();
+ app.use(cors());
+
+ app.get('/task2A', (req, res) => {
+   const a = req.query.a;
+   const b = req.query.b;
+   const result = (parseInt(a) || 0) + (parseInt(b) || 0);
+   res.send( result.toString() );
+ });
+
+ app.listen(3000, () => {
+ console.log('Example app listening on port 3000!');
+ });
+
