d41df74b268924cd2e4fdbb112b5f1984359dc1f
Sergei Andreevich <sergei.andreevich74@yandex.ru>
2016-11-14 02:17:45 +0500

    @username

--- src/index3.js
+++ src/index3.js
@@ -1,4 +1,6 @@
+
 import express from 'express'
+
 import cors from 'cors'
 
 //var express = require('express');
@@ -7,19 +9,15 @@ app.use(cors());
 
 app.get('/sistemnik2C', (req, res) => {
   const username = canonize(req.query.username);
-res.send( username );
+  res.send( username );
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
 
-
-
 function canonize(url) {
-
   const re = /durov|kriasoft|pavel.durov|dan_abramov|kriasoft/i;
-
   const username = url.match(re) || 'tjholowaychuk';
   return '@' + username;
 }
