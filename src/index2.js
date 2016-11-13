f05bb8f3a10d2e01609b0734dd0a7ca9177b13f0
Sergei Andreevich <sergei.andreevich74@yandex.ru>
2016-11-14 03:01:14 +0500

    Фамилия И. О.

--- src/index2.js
+++ src/index2.js
@@ -5,3 +5,28 @@
 ?fullname=Steven Paul Jobs
 Jobs S. P.
 */
+
+import express from 'express'
+
+import cors from 'cors'
+
+var app = express();
+app.use(cors());
+
+app.get('/sistemnik2B', (req, res) => {
+  const fio = req.query.fullname || 'Invalid fullname';
+
+
+  var r = /(\w+)\s(\w+)\s(\w+)/;
+  var re = fio.replace(r, "$3 $1 $2");
+  var txt = re.split(" ");
+  var name1 = txt[1].substr(0,1);
+  var name2 = txt[2].substr(0,1);
+
+  const fullname = txt[0] + " " + name1 + ". " + name2 + ".";
+  res.send( fullname );
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
