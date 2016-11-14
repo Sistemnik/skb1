122f3bb5449d75bf9b23ee9eca5e0020a454d8bb
Sergei Andreevich <sergei.andreevich74@yandex.ru>
2016-11-15 02:21:23 +0500

    Фамилия И. О. (incorrect)

--- src/index2.js
+++ src/index2.js
@@ -1,13 +1,11 @@
 /* Клиент делает GET запрос с параметром Query: ?fullname в виде Имя Отчество Фамилия (или Имя Фамилия или Фамилия), ваша задача сделать вывести инициалы в таком виде: Фамилия И. О. ( или Фамилия И.).
 Результат нужно вывести в виде строки, при неверных входных данных нужно вывести слово Invalid fullname.
-
 Пример 1
 ?fullname=Steven Paul Jobs
 Jobs S. P.
 */
 
 import express from 'express'
-
 import cors from 'cors'
 
 var app = express();
@@ -15,7 +13,6 @@ app.use(cors());
 
 app.get('/sistemnik2B', (req, res) => {
   const fio = req.query.fullname || 'Invalid fullname';
-
 
   var r = /(\w+)\s(\w+)\s(\w+)/;
   var re = fio.replace(r, "$3 $1 $2");
